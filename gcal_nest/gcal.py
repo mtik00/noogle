cb63d3cafda49919f0f56257e8aa70f40e724789
Timothy McFadden <timothy.mcfadden@broadcom.com>
2017-06-21 16:10:28 -0600

    fall back to `q='nest'`

--- gcal_nest/gcal.py
+++ gcal_nest/gcal.py
@@ -82,14 +82,13 @@ def setup(noauth_local_webserver=False):
         )
 
 
-def get_next_events(max_results=10, q_filter=None):
+def get_next_events(max_results=10, q_filter='nest'):
     '''
     Returns a list of events filtered by ``q_filter``.
 
     :param int max_results: The maximum number of results to return
     :param str q: This is the "advanced search syntax" item
     '''
-    q_filter = q_filter or 'nest'
     credentials = get_credentials()
     http = credentials.authorize(httplib2.Http())
     service = discovery.build('calendar', 'v3', http=http)
