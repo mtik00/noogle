59729fea4d1b31f5b92a7e03d7e322d4b849c444
Timothy McFadden <timothy.mcfadden@broadcom.com>
2017-06-21 16:10:05 -0600

    add license to metadata

--- gcal_nest/__init__.py
+++ gcal_nest/__init__.py
@@ -7,4 +7,5 @@ This file holds the project metadata.
 # Metadata ####################################################################
 __author__ = 'Timothy McFadden'
 __creationDate__ = '08-JUN-2017'
+__license__ = 'MIT'
 __version__ = '1.0.0b1'
