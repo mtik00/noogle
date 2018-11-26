a7386bce48d0738251a9faf07f036b113b646a2e
Timothy McFadden <tim@timandjamie.com>
2018-11-25 19:06:02 -0700

    adding circusd services

--- gcal_nest/cli/service.py
+++ gcal_nest/cli/service.py
@@ -11,12 +11,16 @@ from random import SystemRandom as random
 
 import click
 import arrow
+import inflect
 
 from ..gcal import get_next_events
 from ..helpers import format_future_time, print_log
+from ..mailgun import send_message
 
 
 # Globals #####################################################################
+engine = inflect.engine()
+
 @click.group()
 def service():
     '''Run a command in service mode'''
@@ -24,45 +28,78 @@ def service():
 
 
 @service.command()
-def gcal(minutes=5):
+@click.option('-p', '--poll', default=5)
+def gcal(poll):
     '''Look for events in Google and add them to the cache'''
     ctx = click.get_current_context().obj
+    poll *= 60
+    text_lines = []
 
     while True:
         # Grab the next 10 events
-        print_log('Reading events from Google calendar...', nl=False)
-        q_filter = 'nestd' if ctx.debug else 'nest'
+        print_log('GCAL: Reading events from Google calendar...')
+        q_filter = 'nest'
         my_events = get_next_events(max_results=10, q_filter=q_filter)
-        print_log('...done')
+        print_log('GCAL: ...done')
 
         for event in my_events:
             if not ctx.cache.exists(event.event_id):
-                print_log("caching new event: {0}".format(event))
+                message = f"GCAL: caching new event: {event}"
+                print_log(message)
                 ctx.cache.add_event(event)
+                text_lines.append(message)
 
-        print_log('waiting until %s' % format_future_time(minutes))
-        time.sleep(minutes * 60)
+        if text_lines:
+            print_log('sending message')
+            send_message(
+                subject="New {} scheduled".format(engine.plural('event', len(text_lines))),
+                text='\n'.join(text_lines)
+            )
+            text_lines = []
+
+        # Any event that's not completed and not found in our list should be
+        # set to 'removed'.
+
+
+        print_log('GCAL: waiting until %s' % format_future_time(seconds=poll))
+        time.sleep(poll)
 
 
 @service.command()
-def nest(minutes=5):
+@click.option('-p', '--poll', default=5)
+def nest(poll=5):
     '''Wait for and process Nest events'''
     ctx = click.get_current_context().obj
+    poll *= 60
+    text_lines = []
 
     while True:
         events = ctx.cache.waiting()
 
         # Look for any events that are past, but within 2 days.
         two_days_ago = arrow.get(days=-2)
-        events = [event for event in events if arrow.get(event.scheduled_date) <= two_days_ago]
+        events = [
+            event for event in events
+            if (arrow.get(event.scheduled_date) >= two_days_ago)
+              and (arrow.get(event.scheduled_date) <= arrow.now())
+        ]
 
         if events:
-            print_log('%d events waiting...' % len(events), nl=False)
+            print_log('NEST: %d events waiting...' % len(events), nl=False)
 
         for event in events:
-            print_log('...doing %s' % event)
+            text_lines.append(f'NEST:...doing {event}')
+            print_log(f'NEST:...doing {event}')
             ctx.cache.do_event(event)
-            print_log('......done')
+            print_log('NEST: ......done')
 
-        print_log('waiting until %s' % format_future_time(minutes))
-        time.sleep(minutes * 60)
+        if text_lines:
+            print_log('sending message')
+            send_message(
+                subject="New {} processed".format(engine.plural('event', len(text_lines))),
+                text='\n'.join(text_lines)
+            )
+            text_lines = []
+
+        print_log('NEST: waiting until %s' % format_future_time(seconds=poll))
+        time.sleep(poll)
