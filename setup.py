f4a77499a853077b978f60ffe16b20f66144e298
Timothy McFadden <tim@timandjamie.com>
2019-01-03 08:40:05 -0700

    adding dev dependencies

--- setup.py
+++ setup.py
@@ -1,5 +1,5 @@
 #!/usr/bin/env python
-'''obfuscator package setup script.'''
+"""obfuscator package setup script."""
 from __future__ import print_function
 import os
 from setuptools import setup, find_packages
@@ -8,7 +8,7 @@ from setuptools import setup, find_packages
 THIS_DIR = os.path.abspath(os.path.dirname(__file__))
 
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     setup(
         name="gcal_nest",
         version="0.0.0",
@@ -18,7 +18,8 @@ if __name__ == '__main__':
         url="https://github.com/mtik00/gcal_nest",
         download_url=(
             "https://github.com/mtik00/gcal_nest/releases/download/v0.0.0/"
-            "gcal_nest-0.0.0.tar.gz"),
+            "gcal_nest-0.0.0.tar.gz"
+        ),
         install_requires=[
             "google-api-python-client",
             "setuptools",
@@ -27,31 +28,27 @@ if __name__ == '__main__':
             "sqlalchemy-utils",
             "sqlalchemy",
         ],
-        packages=find_packages(exclude=('fabfile',)),
+        extras_require={"dev": ["jinja2", "ruamel.yaml"]},
+        packages=find_packages(exclude=("fabfile",)),
         zip_safe=True,
         include_package_data=True,
         test_suite="tests",
-        license='MIT',
-
+        license="MIT",
         # https://pypi.python.org/pypi?%3Aaction=list_classifiers
         classifiers=[
-            'Development Status :: 3 - Alpha',
-            'Environment :: Console',
-            'License :: OSI Approved :: MIT License',
-            'Programming Language :: Python',
-            'Programming Language :: Python :: 3',
-            'Programming Language :: Python :: 3.7',
-            'Intended Audience :: Developers',
-            'Environment :: Console',
-            'Natural Language :: English',
-            'Operating System :: OS Independent',
-            'Topic :: Home Automation',
-            'Topic :: Internet'
+            "Development Status :: 3 - Alpha",
+            "Environment :: Console",
+            "License :: OSI Approved :: MIT License",
+            "Programming Language :: Python",
+            "Programming Language :: Python :: 3",
+            "Programming Language :: Python :: 3.7",
+            "Intended Audience :: Developers",
+            "Environment :: Console",
+            "Natural Language :: English",
+            "Operating System :: OS Independent",
+            "Topic :: Home Automation",
+            "Topic :: Internet",
         ],
-
         long_description=open(os.path.join(THIS_DIR, "README.md")).read(),
-
-        entry_points={
-            'console_scripts': ['gcal-nest=gcal_nest.cli:cli'],
-        }
+        entry_points={"console_scripts": ["gcal-nest=gcal_nest.cli:cli"]},
     )
