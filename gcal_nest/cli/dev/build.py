c47266e334e99a7ff632b7a0cf391572ebaccd65
Timothy McFadden <tim@timandjamie.com>
2019-01-04 09:04:32 -0700

    finish up build script

--- gcal_nest/cli/dev/build.py
+++ gcal_nest/cli/dev/build.py
@@ -10,15 +10,15 @@ def build():
     Build the utility
     """
     base_dir = os.path.abspath(os.path.join(os.path.dirname(__file__), '..', '..', '..'))
-    conf_dir = os.path.abspath(os.path.join(base_dir, 'conf'))
     instance_dir = os.path.abspath(os.path.join(base_dir, 'instance'))
     outdir = os.path.join(base_dir, '_build')
+    template_dir = os.path.abspath(os.path.join(os.path.dirname(__file__), 'templates'))
 
     site_config_file = os.path.join(instance_dir, 'site.yaml')
     if not os.path.exists(site_config_file):
         click.echo('ERROR: Could not find %s' % site_config_file)
         click.echo('...a sample is located in `conf`')
-        click.echo('...copy `conf/site.yaml` to your instance folder, and modify it as needed')
+        click.echo('...copy `conf/site-sample.yaml` to your instance folder as `site.yaml`, and modify it as needed')
         click.echo('...we think your instance folder is here: ' + instance_dir)
         raise click.Abort()
 
@@ -28,16 +28,16 @@ def build():
         os.makedirs(outdir)
 
     env = Environment(
-        loader=FileSystemLoader(conf_dir),
+        loader=FileSystemLoader(template_dir),
         undefined=StrictUndefined)
 
     ###########################################################################
-    # click.echo('Creating `_build/gcal-nest-systemd.service')
-    # template = env.get_template('gcal-nest-systemd.service.j2')
-    # content = template.render(**options)
-    # with open(os.path.join(outdir, 'gcal-nest-systemd.service'), 'wb') as fh:
-    #     fh.write(content)
-    # click.echo('...done')
+    click.echo('Creating `_build/gcal-nest-systemd.service')
+    template = env.get_template('gcal-nest-systemd.service.j2')
+    content = template.render(**options)
+    with open(os.path.join(outdir, 'gcal-nest-systemd.service'), 'wb') as fh:
+        fh.write(content.encode('utf-8'))
+    click.echo('...done')
     ###########################################################################
 
     ###########################################################################
@@ -45,6 +45,15 @@ def build():
     template = env.get_template('deploy.bash.j2')
     content = template.render(**options)
     with open(os.path.join(outdir, 'deploy.bash'), 'wb') as fh:
+        fh.write(content.encode('utf-8'))
+    click.echo('...done')
+    ###########################################################################
+
+    ###########################################################################
+    click.echo('Creating `_build/circus.ini')
+    template = env.get_template('circus.ini.j2')
+    content = template.render(**options)
+    with open(os.path.join(outdir, 'circus.ini'), 'wb') as fh:
         fh.write(content.encode('utf-8'))
     click.echo('...done')
     ###########################################################################
