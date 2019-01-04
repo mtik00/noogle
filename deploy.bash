086cc77837cba4f9db028b927489f42e35ac6707
Timothy McFadden <tim@timandjamie.com>
2019-01-04 10:35:31 -0700

    making instance dirname configurable

--- deploy.bash
+++ deploy.bash
@@ -1,23 +1,15 @@
 #!/usr/bin/env bash
-# NOTE: This should be run in an already-activate virtual environment on the
+# NOTE: This should be run in an already-activated virtual environment on the
 # target host.
 
 # Make sure we bring in any changes to python #################################
-pip install -e .
+pip install -e .[dev]
 ###############################################################################
 
-# make sure the build directory is clear
-if [[ ! -d "_build" ]]; then
-    mkdir _build
-else
-    rm -rf _build/*
-fi
-
 # build the config files
-# flask build
+gcal-nest dev build
 
-# Upgrade the database
-# flask db upgrade
+# TODO: Upgrade the database?
 
 # bootstrap the built deploy file
 # sudo bash _build/deploy.bash
