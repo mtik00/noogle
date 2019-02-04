9ad7d7b07643b25b47155b2362896f26efe0c6dd
Timothy McFadden <tim@timandjamie.com>
2019-02-03 22:19:37 -0700

    add missing template

---
+++ noogle/cli/dev/templates/env.sh
@@ -0,0 +1,9 @@
+export NEST_PRODUCT_ID=<nest-product-id>
+export NEST_PRODUCT_SECRET=<nest-product-secret>
+
+export MAILGUN_API_KEY=<mailgun-api-key>
+export MAILGUN_DOMAIN_NAME=<mailgun-domain-name>
+export MAILGUN_FROM=<mailgun-from>
+export MAILGUN_TO=<mailgun-to>
+
+eval "$(_NOOGLE_COMPLETE=source noogle)"
