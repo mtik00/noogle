da14516b8531dec8346f397ffb41946cae20e530
Tim McFadden <mtik00@users.noreply.github.com>
2022-01-16 09:40:27 -0700

    (feat) adding skeleton setup script

---
+++ skel.py
@@ -0,0 +1,108 @@
+#!/usr/bin/env python
+
+from pathlib import Path
+
+
+def create_folders() -> None:
+    if not Path(".secrets").exists():
+        Path(".secrets").mkdir()
+
+    for d in ["config", "data", "tokens"]:
+        path = Path(".secrets") / d
+        if not path.exists():
+            print("Creating", str(path))
+            path.mkdir()
+
+
+def create_env() -> None:
+    path = Path(".secrets", "env.sh")
+    if not path.exists():
+        path.write_text(
+            (
+                "export NEST_PRODUCT_ID=\n"
+                "export NEST_PRODUCT_SECRET=\n"
+                "\n"
+                "export MAILGUN_API_KEY=\n"
+                "export MAILGUN_DOMAIN_NAME=\n"
+                "export MAILGUN_FROM=\n"
+                "export MAILGUN_TO="
+            )
+        )
+
+        print(
+            str(path), "has been created.  You must modify set the variables defined."
+        )
+
+
+def create_config() -> None:
+    config_file = Path(".secrets", "config", "noogle.ini")
+    template = Path("noogle", "cli", "dev", "templates", "conf-format.ini")
+    with open(template) as fh:
+        template_string = fh.read()
+
+    if config_file.exists():
+        print("WARNING: Config file already exists")
+        answer = input("...Are you sure you want to overwrite [Yn]? ")
+        if not answer.lower().startswith("y"):
+            return
+
+    use_logfile = input("Use log file [Yn]? ").lower().startswith("y")
+    print("*** Nest Setup")
+    nest_structure = input("...enter the name of the Nest structure: ")
+    nest_thermostat = input(
+        "...enter the name of the thermostat to control (optional): "
+    )
+    nest_eco_temperature = input("...enter the eco temperature [50]: ") or "50"
+    nest_winter_home_temperature = (
+        input("...enter the temperature to set when 'home' in winter [65]: ") or "65"
+    )
+    nest_max_hold = (
+        input("...enter the maximum number of days for 'nest:home' (optional): ") or ""
+    )
+    print("*** Google Setup")
+    gcal_calendar_id = (
+        input("...enter the Calendar ID to watch [primary]: ") or "primary"
+    )
+    default_home_time = (
+        input(
+            "...enter the default start time for 'all day' _home_ events (24-hour time) [6:00]: "
+        )
+        or "6:00"
+    )
+    default_away_time = (
+        input(
+            "...enter the default start time for 'all day' _away_ events (24-hour time) [19:00]: "
+        )
+        or "19:00"
+    )
+    lookback = input("...enter the number of days to look back for events [2]: ") or "2"
+    timezone = input("...enter the timezone of the structure [MST]: ") or "MST"
+
+    new_config = template_string.format(
+        use_logfile="True" if use_logfile else "False",
+        nest_structure=nest_structure,
+        nest_thermostat=nest_thermostat,
+        nest_eco_temperature=nest_eco_temperature,
+        nest_winter_home_temperature=nest_winter_home_temperature,
+        nest_max_hold=nest_max_hold,
+        gcal_calendar_id=gcal_calendar_id,
+        default_home_time=default_home_time,
+        default_away_time=default_away_time,
+        lookback=lookback,
+        timezone=timezone,
+    )
+
+    with open(config_file, "w") as fh:
+        fh.write(new_config)
+
+    print("Config file stored at:", str(config_file))
+
+
+def main():
+    create_folders()
+    create_config()
+    create_env()
+
+
+if __name__ == "__main__":
+    main()
