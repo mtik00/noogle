b9ffc4b44242afecb84794f24ac065212dd9b61d
Tim McFadden <mtik00@users.noreply.github.com>
2022-01-23 10:23:13 -0700

    (refactor) remove config files from skel.py

--- skel.py
+++ skel.py
@@ -18,52 +18,35 @@ def create_folders() -> None:
 
 
 def create_env() -> None:
-    path = Path(".env")
-    if not path.exists():
-        path.write_text(
-            (
-                "NOOGLE_GENERAL__USE_LOGFILE=False\n"
-                "\n"
-                "NOOGLE_NEST__STRUCTURE=\n"
-                "NOOGLE_NEST__ECO_TEMPERATURE=50\n"
-                "NOOGLE_NEST__PRODUCT_ID=\n"
-                "NOOGLE_NEST__PRODUCT_SECRET=\n"
-                "NOOGLE_NEST__WINTER_HOME_MIN_TEMP=65\n"
-                "\n"
-                "NOOGLE_CALENDAR__NAME=primary\n"
-                "NOOGLE_CALENDAR__DEFAULT_HOME_TIME=06:00\n"
-                "NOOGLE_CALENDAR__DEFAULT_AWAY_TIME=18:00\n"
-                "NOOGLE_CALENDAR__LOOKBACK=2\n"
-                "NOOGLE_CALENDAR__TIMEZONE=\n"
-                "\n"
-                "NOOGLE_MAILGUN__API_KEY=\n"
-                "NOOGLE_MAILGUN__DOMAIN_NAME=\n"
-                "NOOGLE_MAILGUN__FROM_ADDRESS=\n"
-                "NOOGLE_MAILGUN__TO_ADDRESS=\n"
-                "\n"
-                "NOOGLE_DATABASE__URI=\n"
+    for path in [Path(".env"), Path(".secrets", ".env")]:
+        if not path.exists():
+            print("Creating", path.resolve())
+            path.write_text(
+                (
+                    "NOOGLE_GENERAL__DEBUG=True\n"
+                    "\n"
+                    "NOOGLE_LOGGING__LOGFILE=\n"
+                    "\n"
+                    "NOOGLE_NEST__STRUCTURE=\n"
+                    "NOOGLE_NEST__ECO_TEMPERATURE=50\n"
+                    "NOOGLE_NEST__PRODUCT_ID=\n"
+                    "NOOGLE_NEST__PRODUCT_SECRET=\n"
+                    "NOOGLE_NEST__WINTER_HOME_MIN_TEMP=65\n"
+                    "\n"
+                    "NOOGLE_CALENDAR__NAME=primary\n"
+                    "NOOGLE_CALENDAR__DEFAULT_HOME_TIME=06:00\n"
+                    "NOOGLE_CALENDAR__DEFAULT_AWAY_TIME=18:00\n"
+                    "NOOGLE_CALENDAR__LOOKBACK=2\n"
+                    "NOOGLE_CALENDAR__TIMEZONE=\n"
+                    "\n"
+                    "NOOGLE_MAILGUN__API_KEY=\n"
+                    "NOOGLE_MAILGUN__DOMAIN_NAME=\n"
+                    "NOOGLE_MAILGUN__FROM_ADDRESS=\n"
+                    "NOOGLE_MAILGUN__TO_ADDRESS=\n"
+                    "\n"
+                    "NOOGLE_DATABASE__URI=sqlite:///.secrets/data/noogle.sqlite3\n"
+                )
             )
-        )
-
-
-def create_env_sh() -> None:
-    path = Path(".secrets", "env.sh")
-    if not path.exists():
-        path.write_text(
-            (
-                "export NEST_PRODUCT_ID=\n"
-                "export NEST_PRODUCT_SECRET=\n"
-                "\n"
-                "export MAILGUN_API_KEY=\n"
-                "export MAILGUN_DOMAIN_NAME=\n"
-                "export MAILGUN_FROM=\n"
-                "export MAILGUN_TO="
-            )
-        )
-
-        print(
-            str(path), "has been created.  You must modify set the variables defined."
-        )
 
 
 def create_logs() -> None:
@@ -88,76 +71,12 @@ def set_permissions() -> None:
     for file in Path(".secrets", "data").glob("*"):
         file.chmod(0o600)
 
-
-def create_config() -> None:
-    config_file = Path(".secrets", "config", "noogle.ini")
-    template = Path("noogle", "cli", "dev", "templates", "conf-format.ini")
-    with open(template) as fh:
-        template_string = fh.read()
-
-    if config_file.exists():
-        print("WARNING: Config file already exists")
-        answer = input("...Are you sure you want to overwrite [Yn]? ")
-        if not answer.lower().startswith("y"):
-            return
-
-    use_logfile = input("Use log file [Yn]? ").lower().startswith("y")
-    print("*** Nest Setup")
-    nest_structure = input("...enter the name of the Nest structure: ")
-    nest_thermostat = input(
-        "...enter the name of the thermostat to control (optional): "
-    )
-    nest_eco_temperature = input("...enter the eco temperature [50]: ") or "50"
-    nest_winter_home_min_temperature = (
-        input("...enter the minimum temperature to set when 'home' in winter [65]: ")
-        or "65"
-    )
-    nest_max_hold = (
-        input("...enter the maximum number of days for 'nest:home' (optional): ") or ""
-    )
-    print("*** Google Setup")
-    gcal_calendar_id = (
-        input("...enter the Calendar ID to watch [primary]: ") or "primary"
-    )
-    default_home_time = (
-        input(
-            "...enter the default start time for 'all day' _home_ events (24-hour time) [6:00]: "
-        )
-        or "6:00"
-    )
-    default_away_time = (
-        input(
-            "...enter the default start time for 'all day' _away_ events (24-hour time) [19:00]: "
-        )
-        or "19:00"
-    )
-    lookback = input("...enter the number of days to look back for events [2]: ") or "2"
-    timezone = input("...enter the timezone of the structure [MST]: ") or "MST"
-
-    new_config = template_string.format(
-        use_logfile="True" if use_logfile else "False",
-        nest_structure=nest_structure,
-        nest_thermostat=nest_thermostat,
-        nest_eco_temperature=nest_eco_temperature,
-        nest_winter_home_min_temperature=nest_winter_home_min_temperature,
-        nest_max_hold=nest_max_hold,
-        gcal_calendar_id=gcal_calendar_id,
-        default_home_time=default_home_time,
-        default_away_time=default_away_time,
-        lookback=lookback,
-        timezone=timezone,
-    )
-
-    with open(config_file, "w") as fh:
-        fh.write(new_config)
-
-    print("Config file stored at:", str(config_file))
+    for file in Path(".secrets").glob("*"):
+        file.chmod(0o600)
 
 
 def main():
     create_folders()
-    create_config()
-    create_env_sh()
     create_env()
     create_logs()
     set_permissions()
