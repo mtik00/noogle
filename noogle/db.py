696d020cd12b4f9b6c364867ec5e1ce128bf72e5
Tim McFadden <mtik00@users.noreply.github.com>
2022-01-23 12:46:10 -0700

    (feat) attempt to chmod the database on init

--- noogle/db.py
+++ noogle/db.py
@@ -1,5 +1,6 @@
 #!/usr/bin/env python3.7
-
+import logging
+from pathlib import Path
 
 from sqlalchemy import create_engine
 from sqlalchemy.ext.declarative import DeclarativeMeta, declarative_base
@@ -18,3 +19,10 @@ def init():
 
     Base.metadata.drop_all(engine)
     Base.metadata.create_all(engine)
+
+    if settings.database.uri.get_secret_value().lower().startswith("sqlite"):
+        try:
+            path = settings.database.uri.get_secret_value().replace("sqlite:///", "")
+            Path(path).chmod(0o600)
+        except Exception as e:
+            logging.error("Could not chmod %s", path)
